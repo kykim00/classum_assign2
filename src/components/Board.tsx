77c7266aa69bacfdaa4399c0952fa989d7d548b1
kykim00 <jeongsaeki@likelion.org>
2022-04-02 23:52:56 +0900

    ✨ 게임 완료 로직 추가

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,30 @@
-import { useSelector } from "react-redux";
-import { gameSelector } from "../stores/gameSetting";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearGame, gameOver, gameSelector } from "../stores/gameSetting";
 import { Cell } from "./Cell";
+import { Modal } from "./common/Modal";
 
 export const Board = () => {
-  const { width, height } = useSelector(gameSelector);
+  const { width, height, board, minesMap } = useSelector(gameSelector);
+  const [isModal, setIsModal] = useState(false);
+  const dispatch = useDispatch();
+
+  const isGameClear = () => {
+    if (board.length === 0) return;
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        if (board[i][j] === "?") {
+          if (!(minesMap[i][j] === 1)) {
+            dispatch(gameOver());
+            return false;
+          }
+        }
+      }
+    }
+    console.log("running");
+    dispatch(clearGame());
+    return true;
+  };
 
   const renderBoard = () => {
     const board = [];
@@ -17,5 +38,25 @@ export const Board = () => {
     return board;
   };
 
-  return <div>{renderBoard()}</div>;
+  useEffect(() => {
+    if (board.length === 0) return;
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        if (board[i][j] === "") return;
+      }
+    }
+    if (isGameClear()) setIsModal(true);
+  }, [board]);
+
+  return (
+    <div>
+      <div>{renderBoard()}</div>
+      {isModal && (
+        <Modal>
+          <h1>CLEAR !!</h1>
+          <button onClick={() => setIsModal(false)}>닫기</button>
+        </Modal>
+      )}
+    </div>
+  );
 };
